476e2dce34c2dd5ad93647243057707a9cd98881
MariaBerbis <maria.berbis.m@gmail.com>
2024-01-28 01:33:35 +0000

    Created component for Meaning

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
+  console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        Hello
         <h2>{props.results.word}</h2>
+        <p>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </p>
       </div>
     );
   } else {
