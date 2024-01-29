2acad737f00e88f9d760e205b4a74dbaac1e6e52
MariaBerbis <maria.berbis.m@gmail.com>
2024-01-29 17:46:59 +0000

    styled synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,6 +5,8 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="synonyms">
+        {" "}
+        Similar:
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
